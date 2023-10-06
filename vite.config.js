35e71e2e7480987b385a59e62635df1645dd21f9
florlopez5 <flolom21@gmail.com>
2023-10-06 18:34:31 -0300

    First commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// const path = require("path");
+import { resolve } from "path";
+export default {
+  root: resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+};
